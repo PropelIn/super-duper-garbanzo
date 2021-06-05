f82ebc8faeb363ac6e71dca5bad3132667bd173f
Daniel Rasmussen <daniel.rasmussen@drgeo.se>
2021-06-05 18:04:22 +0200

    adds propel

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          It's <code>time</code> to propel.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.drgeo.se"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Start
         </a>
       </header>
     </div>
